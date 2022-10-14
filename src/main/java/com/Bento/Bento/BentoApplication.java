6f51ed947611399e0a4ba7ff16f2a2141a151c39
Parminder <paramindersingh0025@gmail.com>
2022-10-13 22:47:11 -0700

    change db configuration

--- src/main/java/com/Bento/Bento/BentoApplication.java
+++ src/main/java/com/Bento/Bento/BentoApplication.java
@@ -2,9 +2,14 @@ package com.Bento.Bento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 //@EnableJpaRepositories
 @EnableMongoRepositories
 public class BentoApplication {
@@ -12,5 +17,23 @@ public class BentoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BentoApplication.class, args);
 	}
+	
+	@Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin("*"); // this allows all origin
+        config.addAllowedHeader("*"); // this allows all headers
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 
 }
--- src/test/java/com/Bento/Bento/BentoApplicationTests.java
+++ src/test/java/com/Bento/Bento/BentoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.Bento.Bento;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BentoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.Bento.Bento;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BentoApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
